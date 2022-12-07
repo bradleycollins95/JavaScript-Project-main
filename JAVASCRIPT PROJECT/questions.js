ea3018420860396f701e2e80266a902549d94817
bradleycollins95 <bradley.collins08@ed.cna.nl.ca>
2022-12-06 21:50:54 -0330

    first commit

---
+++ JAVASCRIPT PROJECT/questions.js
@@ -0,0 +1,103 @@
+const Questions = [
+    {
+      question: "Which record label did The Notorious B.I.G sign with?",
+      answers: {
+        a: "Bad Boy Records",
+        b: "Death Row Records",
+        c: "Sugarhill Records",
+        d: "Def Jam Records"
+      },
+      correctAnswer: "Bad Boy Records"
+    },
+    {
+      question: "Who is the lead singer of Green Day",
+      answers: {
+        a: "Billie Jean Armstrong",
+        b: "Neil Armstrong",
+        c: "Billie Joe Armstrong",
+        d: "Louis Armstrong"
+      },
+      correctAnswer: "Billie Joe Armstrong"
+    },
+    {
+      question: "Who wrote the hit 2007 song 'Our Song' for a talent show?",
+      answers: {
+        a: "Marah Carey",
+        b: "Taylor Swift",
+        c: "Beyonce",
+        d: "Christina Agulara"
+      },
+      correctAnswer: "Taylor Swift"
+    },
+    {
+      question: "Who wrote everyone's moms favorite rap hit 'The Fresh Prince of Bel-Air'",
+      answers: {
+        a: "50 Cent",
+        b: "Usher",
+        c: "Will Smith",
+        d: "Eminem"
+      },
+      correctAnswer: "Will Smith"
+    },
+    {
+      question: "What was the Beatles first single to sell a million copies?",
+      answers: {
+        a: "Hey Jude",
+        b: "I Want To Hold Your Hand",
+        c: "Can't Buy Me Love",
+        d: "She Loves You"
+      },
+      correctAnswer: "She Loves You"
+    },
+    {
+      question: "What is the name of the 2nd studio release by Slipknot?",
+      answers: {
+        a: ".5, The Gray Chapter",
+        b: "Slipknot",
+        c: "We Are Not Your Kind",
+        d: "Iowa"
+      },
+      correctAnswer: "Iowa"
+    },
+    {
+      question: "What is the name of the band who wrote the hit single 'Misery Business'?",
+      answers: {
+        a: "Paramore",
+        b: "Alter Bridge",
+        c: "Creed",
+        d: "Fall Out Boy"
+      },
+      correctAnswer: "Paramore"
+    },
+    {
+      question: "Who is the guitar player in the band 'Guns 'n' Roses'?",
+      answers: {
+        a: "Ace Frehley",
+        b: "Slash",
+        c: "Tony Iommi",
+        d: "Buckethead"
+      },
+      correctAnswer: "Slash"
+    },
+    {
+      question: "What year did the album 'Come Whatever May' by Stone Sour release?",
+      answers: {
+        a: "2001",
+        b: "2002",
+        c: "2003",
+        d: "2004"
+      },
+      correctAnswer: "2004"
+    },
+    {
+      question: "What band featured the song 'Rollin' as the theme song for WWE's 'The Undertaker' at one point?",
+      answers: {
+        a: "Static X",
+        b: "P.O.D.",
+        c: "Limp Bizkit",
+        d: "Papa Roach"
+      },
+      correctAnswer: "Limp Bizkit"
+    }
+    
+  ];
